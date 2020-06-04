cec615bb4dffbdc5b9dfa21c74eecf20f63bdb7f
Jackson Nascimento <jsnascimento1@hotmail.com>
2020-06-04 17:59:55 -0300

    update server js file

--- src/server.js
+++ src/server.js
@@ -1,16 +1,38 @@
 const express = require("express")
 const server =  express()
 
-//Configurar caminhos da minha aplicação
-//Página inicial
-server.get("/", (req, res) => {
+//Configurando pasta pública
+server.use(express.static("public"))
 
-//  rec - requisição (pedido)
-//  res - resposta
-    res.send("Cheguei aqui")
+
+
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
 })
 
 
+
+
+//Configurar caminhos da minha aplicação
+//Página inicial
+//  rec - requisição (pedido)
+//  res - resposta
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+//Rota para página de formulário
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+// ?A parte de Search results é algo dinâmico.Logo, deverá ser
+// ?feito de uma outra forma para que possam ser extraídos dados
+// ?do banco de dados que está recebendo os cadastros dos ecopontos
 
 // !Ligar o servidor
 server.listen(3000)
