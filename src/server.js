4375213ae1c2e297a51840e197f95ff03d6f1a39
Jackson Nascimento <jsnascimento1@hotmail.com>
2020-06-06 15:19:03 -0300

    update server file

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db")
 //Configurando pasta pública
 server.use(express.static("public"))
 
+//!Habilitando o uso do req.body na aplicação
+server.use(express.urlencoded({extended: true}))
+
 
 
 // Utilizando template engine
@@ -30,8 +33,57 @@ server.get("/", (req, res) => {
 
 //Rota para página de formulário
 server.get("/create-point", (req, res) => {
+
+// *Pegando conteudo da url após submissao de cadastro no Banco de dados
+    console.log(req.query)
     return res.render("create-point.html")
 })
+
+// !Escondendo os dados que são mencionados na url ao cadastrar ponto de coleta
+server.post("/savepoint", (req, res) => {
+    console.log(req.body)
+    //Inserindo dados no banco de dados
+    
+
+        // *02 - Inserir dados na tabela
+        
+        const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (?, ?, ?, ?, ?, ?, ?);
+            `
+        
+        const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.items
+        ]
+
+        function afterInsertData(err){            
+            if(err){
+                console.log(err) //*Mostrando o erro que apareceu
+                return res.send("Erro no cadastro!")
+            }
+             console.log("cadastrado com sucesso!")
+             console.log(this)
+             
+             return res.render("create-point.html", {saved: true})
+        }
+
+        db.run(query, values, afterInsertData)
+})
+
+
 
 // ?A parte de Search results é algo dinâmico.Logo, deverá ser
 // ?feito de uma outra forma para que possam ser extraídos dados
@@ -39,11 +91,25 @@ server.get("/create-point", (req, res) => {
 
 server.get("/search", (req, res) => {
 
+    const search = req.query.search
+
+    if(search == ""){
+        //Pesquisa vazia
+        // *Mostra a página html com os dados do banco de dados
+        return res.render("search-results.html", {total: 0})
+    }
+        if(search == ""){
+        //Pesquisa vazia
+        // *Mostra a página html com os dados do banco de dados
+        return res.render("search-results.html", {total: 0})
+    }
+
+    
     //Pegar os dados do banco de dados
 
         // *03 - Consultar os dados da tabela
 
-        db.all(`SELECT * FROM places`, function(err, rows){
+        db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
             if(err){
                 return console.log(err) //*Mostrando o erro que apareceu
             }
